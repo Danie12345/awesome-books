bc4a3168301d7c526b907e3e28eb435d6dfe03fe
Daniel Malo <danielmaloosorio@gmail.com>
2022-04-18 16:32:41 -0500

    Linter errors fix

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,22 @@
 const mainSection = document.querySelector('main');
 const form = document.querySelector('#form');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
 let storedBooks = {};
 
+function updateBooks(book) {
+  storedBooks[book.time] = book;
+  localStorage.setItem('stored-books', JSON.stringify(storedBooks));
+}
+
+function removeBook(book) {
+  delete storedBooks[book.time];
+  localStorage.setItem('stored-books', JSON.stringify(storedBooks));
+}
+
 const bookTemplate = (newBook) => {
-  let div = document.createElement('div');
-  div.setAttribute('name', newBook.time)
+  const div = document.createElement('div');
+  div.setAttribute('name', newBook.time);
   const book = document.createElement('span');
   book.innerHTML = newBook.title;
   const author = document.createElement('span');
@@ -13,12 +25,12 @@ const bookTemplate = (newBook) => {
   removeBtn.innerHTML = 'Remove';
   removeBtn.addEventListener('click', () => {
     mainSection.removeChild(div);
-    removeBook(div);
+    removeBook(newBook);
   });
-  const hr = document.createElement('hr');
   const br1 = document.createElement('br');
   const br2 = document.createElement('br');
   const br3 = document.createElement('br');
+  const hr = document.createElement('hr');
 
   div.appendChild(book);
   div.appendChild(br1);
@@ -29,60 +41,38 @@ const bookTemplate = (newBook) => {
   div.appendChild(hr);
 
   return div;
-}
+};
 
 function getBooks() {
   if (localStorage.getItem('stored-books') === null) {
     localStorage.setItem('stored-books', JSON.stringify(storedBooks));
   } else {
     storedBooks = JSON.parse(localStorage.getItem('stored-books'));
-    for (book in storedBooks) {
-      mainSection.appendChild(bookTemplate(storedBooks[book]));
-    }
+    Object.values(storedBooks).forEach((book) => {
+      mainSection.appendChild(bookTemplate(book));
+    });
   }
-}
-
-function retrieveBooks() {
-  storedBooks = JSON.parse(localStorage.getItem('stored-books'));
-}
-
-function updateBooks(bookHTML, book) {
-  storedBooks[bookHTML.getAttribute('name')] = book;
-  localStorage.setItem('stored-books', JSON.stringify(storedBooks));
-}
-
-function removeBook(book) {
-  delete storedBooks[book.getAttribute('name')];
-  console.log(book.getAttribute('name'));
-  localStorage.setItem('stored-books', JSON.stringify(storedBooks));
 }
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  let title = document.querySelector('#title');
-  let author = document.querySelector('#author');
-
-  if (title.value != '' || author.value != '') {
-    let addBook = {
+  if (title.value !== '' && author.value !== '') {
+    const addBook = {
       title: title.value,
       author: author.value,
       time: new Date().getTime(),
     };
-    let newBook = bookTemplate(addBook);
+    const newBook = bookTemplate(addBook);
     mainSection.appendChild(newBook);
-    updateBooks(newBook, addBook);
-
+    updateBooks(addBook);
     title.value = '';
     author.value = '';
-  } else {
-    if (title.value == '') {
-      alert('What\'s the title of your book?');
-    } else if (author.value == '') {
-      alert('Who wrote that book?');
-    }
-    return;
-  } 
+  } else if (title.value === '') {
+    alert('What\'s the title of your book?');
+  } else if (author.value === '') {
+    alert('Who wrote that book?');
+  }
 });
 
 window.addEventListener('load', getBooks);
