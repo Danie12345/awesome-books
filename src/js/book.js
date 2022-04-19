cea04048cb90d48effc360176454aceb2df74a0c
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-04-19 10:56:56 -0600

    update book.js

--- src/js/book.js
+++ src/js/book.js
@@ -1,3 +1,43 @@
 export class Book {
-  
+    constructor(author, title, parent, section) {
+        this.author = author;
+        this.title = title;
+        this.time = new Date().getTime();
+        this.parent = parent;
+        this.section = section;
+        this.template = this.generateTemplate();
+    }
+
+    getBook() {
+        return this;
+    }
+
+    generateTemplate() {
+        const div = document.createElement('div');
+        div.setAttribute('name', this.time);
+        const book = document.createElement('span');
+        book.innerHTML = this.title;
+        const author = document.createElement('span');
+        author.innerHTML = this.author;
+        const removeBtn = document.createElement('button');
+        removeBtn.innerHTML = 'Remove';
+        removeBtn.addEventListener('click', () => {
+            this.section.removeChild(div);
+            this.parent.delBook(this);
+        });
+        const br1 = document.createElement('br');
+        const br2 = document.createElement('br');
+        const br3 = document.createElement('br');
+        const hr = document.createElement('hr');
+
+        div.appendChild(book);
+        div.appendChild(br1);
+        div.appendChild(author);
+        div.appendChild(br2);
+        div.appendChild(removeBtn);
+        div.appendChild(br3);
+        div.appendChild(hr);
+
+        return div;
+    }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -7,35 +7,6 @@ const title = document.querySelector('#title');
 const author = document.querySelector('#author');
 let shelf = new Bookshelf();
 
-const bookTemplate = (newBook) => {
-  const div = document.createElement('div');
-  div.setAttribute('name', newBook.time);
-  const book = document.createElement('span');
-  book.innerHTML = newBook.title;
-  const author = document.createElement('span');
-  author.innerHTML = newBook.author;
-  const removeBtn = document.createElement('button');
-  removeBtn.innerHTML = 'Remove';
-  removeBtn.addEventListener('click', () => {
-    mainSection.removeChild(div);
-    shelf.delBook(newBook);
-  });
-  const br1 = document.createElement('br');
-  const br2 = document.createElement('br');
-  const br3 = document.createElement('br');
-  const hr = document.createElement('hr');
-
-  div.appendChild(book);
-  div.appendChild(br1);
-  div.appendChild(author);
-  div.appendChild(br2);
-  div.appendChild(removeBtn);
-  div.appendChild(br3);
-  div.appendChild(hr);
-
-  return div;
-};
-
 function getBooks() {
   shelf.retrieveStorage();
   Object.values(shelf.books).forEach((book) => {
@@ -47,14 +18,9 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
 
   if (title.value !== '' && author.value !== '') {
-    const addBook = {
-      title: title.value,
-      author: author.value,
-      time: new Date().getTime(),
-    };
-    const newBook = bookTemplate(addBook);
-    mainSection.appendChild(newBook);
-    shelf.addBook(addBook);
+    const book = new Book(author, title, shelf, mainSection);
+    mainSection.appendChild(book.template);
+    shelf.addBook(book);
     title.value = '';
     author.value = '';
   } else if (title.value === '') {
