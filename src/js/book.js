4ab5cb9c9a772e7d1abe9b7468ec2cd170e9ca04
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-04-19 12:57:40 -0600

    update css

--- src/js/book.js
+++ src/js/book.js
@@ -13,28 +13,19 @@ export default class Book {
     const div = document.createElement('div');
     div.setAttribute('name', this.time);
     const book = document.createElement('span');
-    book.innerHTML = this.title;
-    const author = document.createElement('span');
-    author.innerHTML = this.author;
+    book.innerHTML = this.title + " by " + this.author;
     const removeBtn = document.createElement('button');
     removeBtn.innerHTML = 'Remove';
     removeBtn.addEventListener('click', () => {
       section.removeChild(div);
       shelf.delBook(this);
     });
-    const br1 = document.createElement('br');
-    const br2 = document.createElement('br');
-    const br3 = document.createElement('br');
-    const hr = document.createElement('hr');
+    const div2 = document.createElement('div');
 
     div.appendChild(book);
-    div.appendChild(br1);
-    div.appendChild(author);
-    div.appendChild(br2);
+    div.appendChild(div2);
     div.appendChild(removeBtn);
-    div.appendChild(br3);
-    div.appendChild(hr);
-
+   
     return div;
   }
 }
