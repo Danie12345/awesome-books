70943d45cd19097722e334303b689bc86771b08d
Daniel Malo <danielmaloosorio@gmail.com>
2022-04-19 14:39:01 -0500

    Add styles
    Add styles to wireframe in index.css.

--- src/js/book.js
+++ src/js/book.js
@@ -13,27 +13,16 @@ export default class Book {
     const div = document.createElement('div');
     div.setAttribute('name', this.time);
     const book = document.createElement('span');
-    book.innerHTML = this.title;
-    const author = document.createElement('span');
-    author.innerHTML = this.author;
+    book.innerHTML = `${this.title} by ${this.author}`;
     const removeBtn = document.createElement('button');
     removeBtn.innerHTML = 'Remove';
     removeBtn.addEventListener('click', () => {
       section.removeChild(div);
       shelf.delBook(this);
     });
-    const br1 = document.createElement('br');
-    const br2 = document.createElement('br');
-    const br3 = document.createElement('br');
-    const hr = document.createElement('hr');
 
     div.appendChild(book);
-    div.appendChild(br1);
-    div.appendChild(author);
-    div.appendChild(br2);
     div.appendChild(removeBtn);
-    div.appendChild(br3);
-    div.appendChild(hr);
 
     return div;
   }
